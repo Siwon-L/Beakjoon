8accd94072fa5e2e025738e633356484e76de823
Safari <91936941+Siwon-L@users.noreply.github.com>
2024-12-27 15:52:13 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 520 ms, Memory: 70032 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.swift
@@ -0,0 +1,54 @@
+struct Queue<T> {
+  var memory = [T]()
+  var index = 0
+  
+  var isEmpty: Bool {
+    memory.count < index + 1
+  }
+  
+  mutating func inQueue(element: T) {
+    memory.append(element)
+  }
+  
+  mutating func deQueue() -> T {
+    let v = memory[index]
+    index += 1
+    return v
+  }
+}
+
+let nm = readLine()!.split(separator: " ").map { Int($0)! }
+let n = nm[0]
+let m = nm[1]
+
+var link = Array(repeating: Array(repeating: false, count: n + 1), count: n + 1)
+var visited = Array(repeating: false, count: n + 1)
+
+for _ in 0..<m {
+  let uv = readLine()!.split(separator: " ").map { Int($0)! }
+  let u = uv[0]
+  let v = uv[1]
+  link[u][v] = true
+  link[v][u] = true
+}
+
+func dfs(start: Int) {
+  visited[start] = true
+  
+  for i in 1...n {
+    if visited[i] == false && link[start][i] {
+      dfs(start: i)
+    }
+  }
+}
+
+var result = 0
+
+for i in 1...n {
+  if visited[i] == false {
+    result += 1
+    dfs(start: i)
+  }
+}
+
+print(result)
