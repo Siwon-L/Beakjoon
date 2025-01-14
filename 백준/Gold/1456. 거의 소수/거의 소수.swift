4373d2a520c7fa4d805ce0f2c86b0868d3b0ee8f
Safari <91936941+Siwon-L@users.noreply.github.com>
2025-01-14 18:10:04 +0900

    [Gold V] Title: 거의 소수, Time: 148 ms, Memory: 114248 KB -BaekjoonHub

--- 백준/Gold/1456. 거의 소수/거의 소수.swift
+++ 백준/Gold/1456. 거의 소수/거의 소수.swift
@@ -1,40 +1,31 @@
 import Foundation
 
-let nm = readLine()!.split(separator: " ").map { Double($0)! }
 
-let a = nm[0] < 2 ? 2 : nm[0]
-let b = nm[1]
 
-let limit = Int(sqrt(Double(b)))
-
-var isPrimeNubers = Array(repeating: true, count: limit + 1)
-
-var count = 0
-
-for i in 2..<Int(sqrt(Double(limit)) + 1) {
-  if isPrimeNubers[i] {
-    var j = 2
-    while i * j <= limit {
-      isPrimeNubers[i * j] = false
-      j += 1
+func makePrimes() -> [Bool] {
+  let limit = 10_000_000
+  var primes = Array(repeating: true, count: limit + 1)
+  primes[0...1] = [false, false]
+  
+  for i in 2...Int(sqrt(Double(limit))) + 1 {
+    if primes[i] {
+      var j = i * 2
+      
+      while j <= limit {
+        primes[j] = false
+        j += i
+      }
     }
   }
+  
+  return primes
 }
 
-for i in 1...limit {
-  if i < 2 { continue }
-  if isPrimeNubers[i] {
-    calculate(num: i, min: a, max: b)
-  }
-}
-
-print(count)
-
-func calculate(num: Int, min: Double, max: Double) {
+func calculate(num: Int, min: Double, max: Double, count: inout Int) {
   let left = Int(log(max, base: Double(num)))
   var right = Int(log(min, base: Double(num)))
   right = right < 2 ? 2 : right
-  if (min...max) ~= pow(Double(num), Double(left)) {
+  if left >= right, (min...max) ~= pow(Double(num), Double(left)) {
     if pow(Double(num), Double(right)) < min { right += 1 }
     count += left - right + 1
   }
@@ -43,3 +34,20 @@ func calculate(num: Int, min: Double, max: Double) {
 func log(_ value: Double, base: Double) -> Double {
     return log(value) / log(base)
 }
+
+func solution() -> Int {
+  let ab = readLine()!.split(separator: " ").map {Double($0)!}
+  let (a,b) = (ab[0], ab[1])
+  
+  let primes = makePrimes().enumerated().filter { $0.element }.map(\.offset)
+  var cnt = 0
+  
+  for prime in primes {
+    calculate(num: prime, min: a, max: b, count: &cnt)
+  }
+  
+  return cnt
+}
+
+print(solution())
+
