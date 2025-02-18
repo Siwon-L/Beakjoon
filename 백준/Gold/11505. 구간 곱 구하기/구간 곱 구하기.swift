370b52a147b1d24662d13fb8dc1b811321bfc514
Safari <91936941+Siwon-L@users.noreply.github.com>
2025-02-18 10:27:02 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 488 ms, Memory: 118972 KB -BaekjoonHub

--- 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.swift
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.swift
@@ -1,71 +1,66 @@
-let MOD = 1_000_000_007
-
 struct SegmentTree {
   var tree: [Int]
   var nums: [Int]
   private var N: Int
-  private let calculation: (Int?, Int?, Int?) -> Int
+  private let calculation: (Int?, Int?) -> Int
   
-  init(nums: [Int], calculation: @escaping (Int?, Int?, Int?) -> Int) {
+  init(nums: [Int], calculation: @escaping (Int?, Int?) -> Int) {
     self.nums = nums
     self.N = nums.count
     self.tree = Array(repeating: 0, count: N * 4)
     self.calculation = calculation
-    _ = makeSegmentTree(range: (s: 0, e: N - 1), node: 1)
+    makeSegmentTree(range: (s: 0, e: N-1), node: 1)
   }
   
   @discardableResult
   mutating func makeSegmentTree() -> Int {
-    return makeSegmentTree(range: (s: 0, e: N - 1), node: 1)
+    return makeSegmentTree(range: (s: 0, e: N-1), node: 1)
   }
   
   @discardableResult
   mutating private func makeSegmentTree(range: (s: Int, e: Int), node: Int) -> Int {
     if range.s == range.e {
-      tree[node] = nums[range.s] % MOD
+      tree[node] = nums[range.s]
       return tree[node]
     }
     let mid = (range.s + range.e) / 2
     let left = makeSegmentTree(range: (s: range.s, e: mid), node: node * 2)
-    let right = makeSegmentTree(range: (s: mid + 1, e: range.e), node: node * 2 + 1)
+    let right = makeSegmentTree(range: (s: mid+1, e: range.e), node: node * 2 + 1)
     
-    tree[node] = calculation(nil, left, right) % MOD
+    tree[node] = calculation(left, right)
     return tree[node]
   }
   
   func search(left: Int, right: Int) -> Int? {
-    return search(range: (s: 0, e: N - 1), node: 1, left: left, right: right)
+    return search(range: (s: 0, e: N-1), node: 1, left: left, right: right)
   }
   
   private func search(range: (s: Int, e: Int), node: Int, left: Int, right: Int) -> Int? {
-    // 구간에 겹치지 않으면 nil 반환
     if right < range.s || left > range.e {
       return nil
     }
     
-    // 구간에 완전히 포함되는 경우
     if left <= range.s && right >= range.e {
       return tree[node]
     }
     
     let mid = (range.s + range.e) / 2
-    let leftResult = search(range: (s: range.s, e: mid), node: node * 2, left: left, right: right)
-    let rightResult = search(range: (s: mid + 1, e: range.e), node: node * 2 + 1, left: left, right: right)
+    let leftR = search(range: (s: range.s, e: mid), node: node * 2, left: left, right: right)
+    let rightR = search(range: (s: mid+1, e: range.e), node: node * 2 + 1, left: left, right: right)
     
-    return calculation(nil, leftResult, rightResult) % MOD
+    return calculation(leftR, rightR)
   }
   
   mutating func update(at index: Int, value: Int) {
-    update(range: (s: 0, e: N - 1), node: 1, at: index, newValue: value)
+    update(range: (s: 0, e: N-1), node: 1, at: index, newValue: value)
     nums[index] = value
   }
   
-  // 재귀적으로 구간에 포함된 노드들의 값을 다시 계산
   mutating private func update(range: (s: Int, e: Int), node: Int, at index: Int, newValue: Int) {
-    if index < range.s || index > range.e { return }
+    guard (range.s...range.e) ~= index else { return }
     
     if range.s == range.e {
-      tree[node] = newValue % MOD
+      tree[node] = newValue
       return
     }
     
@@ -75,26 +70,26 @@ struct SegmentTree {
     } else {
       update(range: (s: mid + 1, e: range.e), node: node * 2 + 1, at: index, newValue: newValue)
     }
-    tree[node] = (tree[node * 2] * tree[node * 2 + 1]) % MOD
+    tree[node] = calculation(tree[node * 2], tree[node * 2 + 1])
   }
 }
-
-let inputLine = readLine()!.split(separator: " ").compactMap { Int($0) }
-let n = inputLine[0]
-let m = inputLine[1]
-let k = inputLine[2]
+let nmk = readLine()!.split(separator: " ").compactMap { Int($0) }
+let n = nmk[0]
+let m = nmk[1]
+let k = nmk[2]
 
 var nums: [Int] = []
+
 for _ in 0..<n {
-  let num = Int(readLine()!)!
-  nums.append(num)
+  let input = Int(readLine()!)!
+  nums.append(input)
 }
 
-var segmentTree = SegmentTree(nums: nums) { _, left, right in
-  return ((left ?? 1) * (right ?? 1)) % MOD
+var segmentTree = SegmentTree(nums: nums) { left, right in
+  return ((left ?? 1) * (right ?? 1)) % 1_000_000_007
 }
-
 var results: [Int] = []
+
 for _ in 0..<(m + k) {
   let abc = readLine()!.split(separator: " ").compactMap { Int($0) }
   let a = abc[0]
@@ -102,11 +97,15 @@ for _ in 0..<(m + k) {
   let c = abc[2]
   
   if a == 1 {
-    segmentTree.update(at: b - 1, value: c)
-  } else {
-    if let result = segmentTree.search(left: b - 1, right: c - 1) {
-      results.append(result)
+    if segmentTree.nums[b-1] == 0 {
+      segmentTree.nums[b-1] = c
+      segmentTree.makeSegmentTree()
+    } else {
+      segmentTree.update(at: b-1, value: c)
     }
+  } else {
+    guard let result = segmentTree.search(left: b-1, right: c-1) else { continue }
+    results.append(result)
   }
 }
 
