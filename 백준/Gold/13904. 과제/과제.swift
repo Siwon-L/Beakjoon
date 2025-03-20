d390fb381e7df711f216eac798c9e14027f74fd8
Safari <91936941+Siwon-L@users.noreply.github.com>
2025-03-20 17:12:58 +0900

    [Gold III] Title: 과제, Time: 12 ms, Memory: 79520 KB -BaekjoonHub

---
+++ 백준/Gold/13904. 과제/과제.swift
@@ -0,0 +1,98 @@
+import Foundation
+
+struct Heap<T> {
+  private var elements: [T] = []
+  private let sortFunction: (T, T) -> Bool
+  
+  var isEmpty: Bool {
+    elements.count < 2
+  }
+  
+  var values: [T] {
+    Array(elements[1..<elements.count])
+  }
+  
+  init(sortFunction: @escaping (T, T) -> Bool) {
+    self.sortFunction = sortFunction
+  }
+  
+  mutating func insert(_ element: T) {
+    if elements.isEmpty {
+      elements.append(element)
+      elements.append(element)
+      return
+    }
+    
+    elements.append(element)
+    moveUp(insertIndex: elements.count - 1)
+  }
+  
+  mutating func pop() -> T? {
+    if isEmpty { return nil }
+    
+    let returnValue = elements[1]
+    elements.swapAt(1, elements.count - 1)
+    elements.removeLast()
+    
+    moveDown(popIndex: 1)
+    return returnValue
+  }
+  
+  mutating private func moveUp(insertIndex: Int) {
+    var index = insertIndex
+    while index > 1, sortFunction(elements[index], elements[index/2]) {
+      elements.swapAt(index, index/2)
+      index = index/2
+    }
+  }
+  
+  mutating private func moveDown(popIndex: Int) {
+    var index = popIndex
+    let leftIndex = index * 2
+    let rightIndex = index * 2 + 1
+    
+    if leftIndex < elements.count, sortFunction(elements[leftIndex], elements[index]) {
+      index = leftIndex
+    }
+    
+    if rightIndex < elements.count, sortFunction(elements[rightIndex], elements[index]) {
+      index = rightIndex
+    }
+    
+    if index != popIndex {
+      elements.swapAt(index, popIndex)
+      moveDown(popIndex: index)
+    }
+  }
+}
+
+func solution() {
+    let n = Int(readLine()!)!
+    var heap = Heap<(d: Int, w: Int)> { $0.w > $1.w }
+    for _ in 0..<n {
+        let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+        heap.insert((input[0], input[1]))
+    }
+    var visited = Array(repeating: false, count: 1001)
+    var result = 0
+    visited[0] = true
+    while let output = heap.pop() {
+      if !visited[output.d] {
+        visited[output.d] = true
+        result += output.w
+      } else {
+        var j = output.d - 1
+        while j > 0 {
+          if !visited[j] {
+            visited[j] = true
+            result += output.w
+            break
+          }
+          j -= 1
+        }
+      }
+    }
+    print(result)
+}
+
+solution()
