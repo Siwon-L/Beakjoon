239b9c74460587ebaf46e686fc51a60188196e72
Safari <91936941+Siwon-L@users.noreply.github.com>
2025-04-18 17:26:34 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func solution(n: Int) {
+    var p = [0]
+    p += readLine()!.split(separator: " ").compactMap { Int($0) }
+    var dp = p
+    for i in 1...n {
+        for j in 1..<i {
+            dp[i] = min(dp[i], dp[i-j] + p[j])
+        }
+        
+    }
+    print(dp[n])
+}
+
+let n = Int(readLine()!)!
+solution(n: n)
