86f36815aac539ebadd1774add551f890e83633c
Safari <91936941+Siwon-L@users.noreply.github.com>
2025-07-19 14:24:09 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 16 ms, Memory: 87324 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+func solution() {
+    var dp = Array(repeating: 0, count: 1000001)
+
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+
+    for i in 4...1000000 {
+    dp[i] = (dp[i-1] + dp[i-2] + dp[i-3]) % 1000000009
+    }
+
+    let t = Int(readLine()!)!
+
+    for _ in 0..<t {
+        let n = Int(readLine()!)!
+        print(dp[n])
+    }
+}
+
+solution()
