e6ec184a58e167fa763ea6f3678f47c2ca8b0570
Safari <91936941+Siwon-L@users.noreply.github.com>
2025-11-18 17:24:51 +0900

    [Silver III] Title: N과 M (2), Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.swift
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.swift
@@ -3,32 +3,16 @@ let n = nm[0]
 let m = nm[1]
 
 let numbers = Array(1...n)
-var visited = Array(repeating: false, count: n)
 var results = [[String]]()
 
-makePermutation(result: [])
+makePermutation(start: 0, result: [])
 
-results = results.map {
-    return $0.sorted(by: <)
-}
-
-Set(results).sorted {
-    let frist = $0.joined(separator: "")
-    let secend = $1.joined(separator: "")
-    return frist < secend
-}.forEach {
-    print($0.joined(separator: " "))
-}
-
-func makePermutation(result: [String]) {
+func makePermutation(start index: Int, result: [String]) {
     if result.count == m {
-        results.append(result)
+        print(result.joined(separator: " "))
     }
-    for i in 0..<n {
-        if !visited[i] {
-            visited[i] = true
-            makePermutation(result: result + ["\(numbers[i])"])
-            visited[i] = false
-        }
+    for i in index..<n {
+        makePermutation(start: i + 1, result: result + ["\(numbers[i])"])
+
     }
 }
